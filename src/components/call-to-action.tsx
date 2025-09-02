e67e8dcfd20b831bc8f9aa3aac6c62cd8bb47179
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-09-02 18:20:33 +0500

    created action

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button'
 
 const CallTOAction = () => {
     return (
-        <div className='w-full h-auto flex justify-center items-center py-10 px-10'>
+        <div className='w-full h-auto flex justify-center items-center py-6 px-10'>
             <div className='w-full flex justify-center min-h-[150px] items-center py-10 px-10 relative'>
                 <div className='absolute top-0 left-0 -z-10 w-full h-auto rounded-4xl bg-green-900/90 mix-blend-multiply'>
                     <div className='w-full h-auto grid-background-2 rounded-4xl min-h-[150px] flex justify-between items-center'>
@@ -13,7 +13,7 @@ const CallTOAction = () => {
                     <h2 className='text-4xl text-white font-medium'>
                         Ready to manage finance easily?</h2>
 
-                    <Button className='hover:bg-secondary/80 text-lg hover:border hover:border-secondary/80 hover:text-primary px-14 py-8 bg-secondary text-primary '>Get Started</Button>
+                    <Button className='hover:bg-secondary/80 text-xl font-semibold hover:border hover:border-secondary/80 hover:text-primary px-10 py-8 bg-secondary text-primary '>Get Started</Button>
 
                 </div>
             </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CallTOAction from './call-to-action'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
@@ -7,8 +8,28 @@ const Footer = () => {
             <div className='w-full h-auto flex justify-center flex-col gap-6 items-center rounded-4xl bg-primary/10'>
                 <CallTOAction />
 
-                <div className='w-full h-auto md:py-14 py-8 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5'>
+                <div className='flex flex-col w-full h-auto justify-center items-center px-3'>
+                    <div className='lg:w-[90%] w-full h-auto grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5'>
+                        <div className='left flex flex-col gap-3'>
+                            <div className='logo w-auto h-[70px] flex items-center'>
+                                <Image src={'/images/logo.png'} alt='Logo' width={150} height={100} />
+                            </div>
 
+                            <p className=''>Earn from your blog without overwhelming your audience with ads. Publisher Authority helps you connect with brands, sponsors, and loyal readers — so you can grow with confidence.</p>
+                        </div>
+                        <div className="middle w-auto flex justify-center">
+                            <div className='flex flex-col gap-3 pt-4 text-left w-full'>
+                                <h4 className='text-2xl font-medium'>Quick Links</h4>
+
+                                <ul className='w-auto'>
+                                    <li className='space-y-1'>Home</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='copy-right flex justify-center items-center py-8'>
+                        <p className='text-primary'>Copyright © 2025 <span className='underline'>Publisher Authority</span>. All Rights Reserved</p>
+                    </div>
                 </div>
             </div>
         </div>
